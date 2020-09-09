299f9d8eb5b4b9e710978a0a882bb82ba76a5b66
Jawad Fadel <jfadel@sabis.net>
2020-09-09 20:41:45 +0300

    Add Top Bar styles

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+/* Show/hide dropdown */
+document.querySelector(".header-top-bar .service-hilfe button").addEventListener("click", () => {
+  document.querySelector(".header-top-bar .service-hilfe ul").classList.toggle("dropdown-visible");
+})

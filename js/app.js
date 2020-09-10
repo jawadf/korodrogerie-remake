b93307651ece9632555d35d1ed297766ce4278a5
Jawad Fadel <jfadel@sabis.net>
2020-09-10 21:28:39 +0300

    update header styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-
-/* Show/hide dropdown */
+/* Show/hide the header dropdown */
 document.querySelector(".header-top-bar .service-hilfe button").addEventListener("click", () => {
   document.querySelector(".header-top-bar .service-hilfe ul").classList.toggle("dropdown-visible");
-})
+  event.stopPropagation();
+}, false);
+
+/* When clicking anywhere else, close the header dropdown */
+window.addEventListener("click", () => {
+    document.querySelector(".header-top-bar .service-hilfe ul").classList.remove("dropdown-visible");
+  }, false);
